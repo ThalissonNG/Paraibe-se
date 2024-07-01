a20ce514969e5220d2ba7b9b7a1fc5f58f4f6c0a
ThalissonM <manoelthalisson28@gmail.com>
2024-07-01 11:47:38 -0300

    Otimização de scripts
    Scripts moisaco e passaportemanager alterados para diminuir o tamanho dele e deixar mais intuitivo o uso

--- Turistar/Assets/Script/Mosaico.cs
+++ Turistar/Assets/Script/Mosaico.cs
@@ -6,91 +6,31 @@ using UnityEngine;
 public class Mosaico : MonoBehaviour
 {
     [SerializeField] private GameObject[] mosaico = new GameObject[9];
-    public int ValorDoMosaico = -1;
-
-    [SerializeField] private int ValorMuseuArtePopular;
-    [SerializeField] private int ValorMonumentoFarra;
-    [SerializeField] private int ValorMuseuDeAlgodao;
-    [SerializeField] private int ValorParqueDoPovo;
-    [SerializeField] private int ValorCatedral;
-    [SerializeField] private int ValorBarDoCuscuz;
-    [SerializeField] private int ValorMuseuDigital;
-    [SerializeField] private int ValorParqueDaCrianca;
-    [SerializeField] private int ValorPioneiros;
+    [SerializeField] private int[] valores = new int[9];
 
     void Start()
     {
-        ValorMuseuArtePopular = PlayerPrefs.GetInt("museudeartepopular");
-        ValorBarDoCuscuz = PlayerPrefs.GetInt("bardocuscuz");
-        ValorCatedral = PlayerPrefs.GetInt("catedral");
-        ValorMonumentoFarra = PlayerPrefs.GetInt("farradabudega");
-        ValorMuseuDigital = PlayerPrefs.GetInt("museudigital");
-        ValorMuseuDeAlgodao = PlayerPrefs.GetInt("museudoalgodao");
-        ValorParqueDaCrianca = PlayerPrefs.GetInt("parquedacrianca");
-        ValorParqueDoPovo = PlayerPrefs.GetInt("parquedopovo");
-        ValorPioneiros = PlayerPrefs.GetInt("pioneiros");
+        valores[0] = PlayerPrefs.GetInt("museudeartepopular");
+        valores[1] = PlayerPrefs.GetInt("bardocuscuz");
+        valores[2] = PlayerPrefs.GetInt("catedral");
+        valores[3] = PlayerPrefs.GetInt("farradabudega");
+        valores[4] = PlayerPrefs.GetInt("museudigital");
+        valores[5] = PlayerPrefs.GetInt("museudoalgodao");
+        valores[6] = PlayerPrefs.GetInt("parquedacrianca");
+        valores[7] = PlayerPrefs.GetInt("parquedopovo");
+        valores[8] = PlayerPrefs.GetInt("pioneiros");
     }
     // Update is called once per frame
     void Update()
     {
-        if (ValorMuseuArtePopular == 1)
-        {
-            mosaico[0].SetActive(true);
-        }
-        if (ValorBarDoCuscuz == 1)
-        {
-            mosaico[1].SetActive(true);
-        }
-        if (ValorCatedral == 1)
-        {
-            mosaico[2].SetActive(true);
-        }
-        if (ValorMonumentoFarra == 1)
-        {
-            mosaico[3].SetActive(true);
-        }
-        if (ValorMuseuDigital == 1)
-        {
-            mosaico[4].SetActive(true);
-        }
-        if (ValorMuseuDeAlgodao == 1)
-        {
-            mosaico[5].SetActive(true);
-        }
-        if (ValorParqueDaCrianca == 1)
-        {
-            mosaico[6].SetActive(true);
-        }
-        if (ValorParqueDoPovo == 1)
-        {
-            mosaico[7].SetActive(true);
-        }
-        if (ValorPioneiros == 1)
-        {
-            mosaico[8].SetActive(true);
-        }
 
-
-
-        //if (Input.GetMouseButtonDown(0)) 
-        //{
-        //    Debug.Log("apertoui");
-        //    ValorDoMosaico++;
-        //}
-        //if (ValorDoMosaico > 9)
-        //{
-        //    ValorDoMosaico = 10; 
-        //}
-        //else
-        //{
-        //    for (int i = 0; i < mosaico.Length; i++)
-        //    {
-        //        if (i == ValorDoMosaico)
-        //        {
-        //            mosaico[i].SetActive(true);
-        //        }
-        //    }
-        //}
+        for (int i = 0; i < mosaico.Length; i++)
+        {
+            if (valores[i] == 1)
+            {
+                mosaico[i].SetActive(true);
+            }
+        }
     }
 
 }
--- Turistar/Assets/Script/PassaporteManager.cs
+++ Turistar/Assets/Script/PassaporteManager.cs
@@ -4,75 +4,95 @@ using UnityEngine;
 
 public class PassaporteManager : MonoBehaviour
 {
-    [SerializeField] private int ValorMuseuArtePopular;
-    [SerializeField] private int ValorMonumentoFarra;
-    [SerializeField] private int ValorMuseuDeAlgodao;
-    [SerializeField] private int ValorParqueDoPovo;
-    [SerializeField] private int ValorCatedral;
-    [SerializeField] private int ValorBarDoCuscuz;
-    [SerializeField] private int ValorMuseuDigital;
-    [SerializeField] private int ValorParqueDaCrianca;
-    [SerializeField] private int ValorPioneiros;
+    [SerializeField] private int[] valores = new int[9];
+    [SerializeField] private GameObject[] carimbos = new GameObject[9];
+    //[SerializeField] private int ValorMuseuArtePopular;
+    //[SerializeField] private int ValorMonumentoFarra;
+    //[SerializeField] private int ValorMuseuDeAlgodao;
+    //[SerializeField] private int ValorParqueDoPovo;
+    //[SerializeField] private int ValorCatedral;
+    //[SerializeField] private int ValorBarDoCuscuz;
+    //[SerializeField] private int ValorMuseuDigital;
+    //[SerializeField] private int ValorParqueDaCrianca;
+    //[SerializeField] private int ValorPioneiros;
 
-    [SerializeField] private GameObject CarimboMuseuDeArte;
-    [SerializeField] private GameObject CarimboMonumentoFarra;
-    [SerializeField] private GameObject CarimboMuseuDeAlgodao;
-    [SerializeField] private GameObject CarimboParqueDoPovo;
-    [SerializeField] private GameObject CarimboCatedral;
-    [SerializeField] private GameObject CarimboBarDoCuscuz;
-    [SerializeField] private GameObject CarimboMuseuDigital;
-    [SerializeField] private GameObject CarimboParqueDaCrianca;
-    [SerializeField] private GameObject CarimboPioneiros;
+    //[SerializeField] private GameObject CarimboMuseuDeArte;
+    //[SerializeField] private GameObject CarimboMonumentoFarra;
+    //[SerializeField] private GameObject CarimboMuseuDeAlgodao;
+    //[SerializeField] private GameObject CarimboParqueDoPovo;
+    //[SerializeField] private GameObject CarimboCatedral;
+    //[SerializeField] private GameObject CarimboBarDoCuscuz;
+    //[SerializeField] private GameObject CarimboMuseuDigital;
+    //[SerializeField] private GameObject CarimboParqueDaCrianca;
+    //[SerializeField] private GameObject CarimboPioneiros;
     void Start()
     {
-        ValorMuseuArtePopular = PlayerPrefs.GetInt("museudeartepopular");
-        ValorBarDoCuscuz = PlayerPrefs.GetInt("bardocuscuz");
-        ValorCatedral = PlayerPrefs.GetInt("catedral");
-        ValorMonumentoFarra = PlayerPrefs.GetInt("farradabudega");
-        ValorMuseuDigital = PlayerPrefs.GetInt("museudigital");
-        ValorMuseuDeAlgodao = PlayerPrefs.GetInt("museudoalgodao");
-        ValorParqueDaCrianca = PlayerPrefs.GetInt("parquedacrianca");
-        ValorParqueDoPovo = PlayerPrefs.GetInt("parquedopovo");
-        ValorPioneiros = PlayerPrefs.GetInt("pioneiros");
+        valores[0] = PlayerPrefs.GetInt("museudeartepopular");
+        valores[1] = PlayerPrefs.GetInt("bardocuscuz");
+        valores[2] = PlayerPrefs.GetInt("catedral");
+        valores[3] = PlayerPrefs.GetInt("farradabudega");
+        valores[4] = PlayerPrefs.GetInt("museudigital");
+        valores[5] = PlayerPrefs.GetInt("museudoalgodao");
+        valores[6] = PlayerPrefs.GetInt("parquedacrianca");
+        valores[7] = PlayerPrefs.GetInt("parquedopovo");
+        valores[8] = PlayerPrefs.GetInt("pioneiros");
+
+        //ValorMuseuArtePopular = PlayerPrefs.GetInt("museudeartepopular");
+        //ValorBarDoCuscuz = PlayerPrefs.GetInt("bardocuscuz");
+        //ValorCatedral = PlayerPrefs.GetInt("catedral");
+        //ValorMonumentoFarra = PlayerPrefs.GetInt("farradabudega");
+        //ValorMuseuDigital = PlayerPrefs.GetInt("museudigital");
+        //ValorMuseuDeAlgodao = PlayerPrefs.GetInt("museudoalgodao");
+        //ValorParqueDaCrianca = PlayerPrefs.GetInt("parquedacrianca");
+        //ValorParqueDoPovo = PlayerPrefs.GetInt("parquedopovo");
+        //ValorPioneiros = PlayerPrefs.GetInt("pioneiros");
     }
 
     void Update()
     {
-        if(ValorMuseuArtePopular == 1)
+        for (int i = 0; i < carimbos.Length; i++)
         {
-            CarimboMuseuDeArte.SetActive(true);
+            if (valores[i] == 1)
+            {
+                carimbos[i].SetActive(true);
+            }
         }
-        if (ValorBarDoCuscuz == 1)
-        {
-            CarimboBarDoCuscuz.SetActive(true);      
-        }
-        if (ValorCatedral == 1)
-        {
-            CarimboCatedral.SetActive(true);
-        }
-        if (ValorMonumentoFarra == 1)
-        {
-            CarimboMonumentoFarra.SetActive(true);
-        }
-        if (ValorMuseuDigital == 1)
-        {
-            CarimboMuseuDigital.SetActive(true);
-        }
-        if (ValorMuseuDeAlgodao == 1)
-        {
-            CarimboMuseuDeAlgodao.SetActive(true);
-        }
-        if (ValorParqueDaCrianca == 1)
-        {
-            CarimboParqueDaCrianca.SetActive(true);
-        }
-        if (ValorParqueDoPovo == 1)
-        {
-            CarimboParqueDoPovo.SetActive(true);
-        }
-        if (ValorPioneiros == 1)
-        {
-            CarimboPioneiros.SetActive(true);
-        }
+
+        //if(ValorMuseuArtePopular == 1)
+        //{
+        //    CarimboMuseuDeArte.SetActive(true);
+        //}
+        //if (ValorBarDoCuscuz == 1)
+        //{
+        //    CarimboBarDoCuscuz.SetActive(true);      
+        //}
+        //if (ValorCatedral == 1)
+        //{
+        //    CarimboCatedral.SetActive(true);
+        //}
+        //if (ValorMonumentoFarra == 1)
+        //{
+        //    CarimboMonumentoFarra.SetActive(true);
+        //}
+        //if (ValorMuseuDigital == 1)
+        //{
+        //    CarimboMuseuDigital.SetActive(true);
+        //}
+        //if (ValorMuseuDeAlgodao == 1)
+        //{
+        //    CarimboMuseuDeAlgodao.SetActive(true);
+        //}
+        //if (ValorParqueDaCrianca == 1)
+        //{
+        //    CarimboParqueDaCrianca.SetActive(true);
+        //}
+        //if (ValorParqueDoPovo == 1)
+        //{
+        //    CarimboParqueDoPovo.SetActive(true);
+        //}
+        //if (ValorPioneiros == 1)
+        //{
+        //    CarimboPioneiros.SetActive(true);
+        //}
     }
 }
